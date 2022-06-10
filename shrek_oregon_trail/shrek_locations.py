20fecc2c387ac18c310a5996f8a0ef3c05c53a24
Michael Samson <Msamson09@users.noreply.github.com>
2022-06-10 19:58:42 +0000

    Shrek Survival Game w/Instructions done

---
+++ shrek_oregon_trail/shrek_locations.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+
+location = [
+        { "swamp" : "swamp places"},
+        { "castle" : "castle places"},
+        {"dragon\'s lair" : "dragon places"}
+        ]
---
+++ shrek_oregon_trail/shrek_main.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+"""Shrek themed survival game.  Survive the swamp and other locations"""
+
+from shrek_locations import location
+
+def showInstructions():
+    print('''
+    Shrek Survival Game
+    -------------------
+    Commands:
+        go [direction]
+        get [item]
+    ''')
+
+showInstructions()
