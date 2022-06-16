ba05d4b14171582bbad957e3c0b4a36b866affc7
Michael Samson <Msamson09@users.noreply.github.com>
2022-06-16 19:08:00 +0000

    project3 functional

---
+++ project3/project3_main.py
@@ -0,0 +1,52 @@
+    # new files to be put into each directory
+    new_docs = ["dorothy.txt", "scarecrow.txt", "lion.txt", "tin_man.txt"]
+
+    # harvest private key for all 3 servers
+    mykey = paramiko.RSAKey.from_private_key_file("/home/student/.ssh/id_rsa")
+
+    # input for any new users to be created
+    new_users = input("What user(s) would you like to add?\n>").split(" ")
+
+    # loop across the collection credz
+    for server in servers:
+        ## create a session object
+        sshsession = paramiko.SSHClient()
+
+        ## add host key policy
+        sshsession.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+        ## display our connections
+        print("Connecting to... " + server.get("un") + "@" + server.get("ip"))
+
+        ## make a connection
+        sshsession.connect(hostname=server.get("ip"), username=server.get("un"), pkey=mykey)
+
+        # loops through the new_dirs list
+        for dirs in new_dirs:
+            # makes a directory in /etc/skel/ for each of the items in the list
+            sshsession.exec_command(f"sudo mkdir /etc/skel/{dirs}/")
+            # loops through the new_docs list
+            for doc in new_docs:
+                # adds each file from new_docs into each of the directories in new_dirs
+                sshsession.exec_command(f"sudo touch /etc/skel/{dirs}/{doc}")
+                # modifies authorities on each of the new files to rwxr-xr-x
+                sshsession.exec_command(f"sudo chmod 0755 /etc/skel/{dirs}/{doc}")
+
+
+
+        # for every new user that was input, adding that user to all three servers
+        for user in new_users:
+            sshsession.exec_command("sudo useradd " + user)
+
+        ## list the contents of each home directory
+        sessin, sessout, sesserr = sshsession.exec_command("ls /home/" + server.get("un"))
+
+        ## display output
+        print(sessout.read().decode('utf-8'))
+
+        ## close/cleanup SSH connection
+        sshsession.close()
+
+
+if __name__ == "__main__":
+    main()
