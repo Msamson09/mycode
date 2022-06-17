632474f97dbe09e91b6ab17b07cfa16917577c60
Michael Samson <Msamson09@users.noreply.github.com>
2022-06-17 15:04:57 +0000

    finished project3

--- project3/project3_main.py
+++ project3/project3_main.py
@@ -1,25 +1,23 @@
 #!/usr/bin/python3
 """This script is used to add a user, create directories inside the home directory
-   and then add files to those directories across three separate servers"""
+   and then add files to those directories across three separate servers from a JSON file"""
 
 import paramiko
 import os
 import json
+import datetime
 
 def main():
     """Our runtime code that calls other functions"""
-    # list of servers
-    servers = [
-             {"un": "bender", "ip": "10.10.2.3"}, 
-             {"un": "zoidberg", "ip": "10.10.2.5"},
-             {"un": "fry", "ip": "10.10.2.4"}
-            ]
+    # list of servers pulled in from JSON file
+    with open("server_list.json") as server_list:
+        servers = json.load(server_list)
 
-    # new directories to be added
-    new_dirs = ["lions", "tigers", "bears", "oh_my"]
+    # new directories being pulled in from JSON file and set as a variable
+    new_dirs = servers["new_dirs"]
 
-    # new files to be put into each directory
-    new_docs = ["dorothy.txt", "scarecrow.txt", "lion.txt", "tin_man.txt"]
+    # new files being pulled in from JSON file and set as a variable
+    new_docs = servers["new_docs"]
 
     # harvest private key for all 3 servers
     mykey = paramiko.RSAKey.from_private_key_file("/home/student/.ssh/id_rsa")
@@ -27,8 +25,8 @@ def main():
     # input for any new users to be created
     new_users = input("What user(s) would you like to add?\n>").split(" ")
 
-    # loop across the collection credz
-    for server in servers:
+    # loop across the collection servers as it is pulled it from JSON and set as a variable
+    for server in servers["servers"]:
         ## create a session object
         sshsession = paramiko.SSHClient()
 
@@ -52,17 +50,28 @@ def main():
                 # modifies authorities on each of the new files to rwxr-xr-x
                 sshsession.exec_command(f"sudo chmod 0755 /etc/skel/{dirs}/{doc}")
 
-
-
         # for every new user that was input, adding that user to all three servers
         for user in new_users:
-            sshsession.exec_command("sudo useradd " + user)
+            sshsession.exec_command("sudo useradd -m " + user)
 
         ## list the contents of each home directory
-        sessin, sessout, sesserr = sshsession.exec_command("ls /home/" + server.get("un"))
+        sessin, sessout, sesserr = sshsession.exec_command("ls /home/")
 
-        ## display output
-        print(sessout.read().decode('utf-8'))
+        # converts datetime into a string for later use
+        x = str(datetime.datetime.now())
+
+        ## display output to a file, log.txt
+        with open("log.txt", "a") as log:
+            # loops through new_users
+            for user in new_users:
+                # runs ls for the home directory of all new users
+                sessin, sessout, sesserr = sshsession.exec_command(f"ls /home/{user}")
+                # writes the current datetime
+                log.write(x)
+                # writes username
+                log.write(f"\n{user}: \n")
+                #writes the results from the ls command
+                log.write(sessout.read().decode('utf-8'))
 
         ## close/cleanup SSH connection
         sshsession.close()
